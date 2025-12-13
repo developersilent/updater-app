4212ec77b34c4ad97f0a22641d45d9a9478ee4e6
developersilent <developersilent101@gmail.com>
2025-12-13 09:47:49 +0000

    server checks

--- lib/eden.ts
+++ lib/eden.ts
@@ -2,4 +2,4 @@ import { treaty } from "@elysiajs/eden";
 import type { app } from "../app/api/[[...slugs]]/route";
 
 // this require .api to enter /api prefix
-export const api = treaty<app>("localhost:3000").api;
+export const api = treaty<app>(process.env.NEXT_PUBLIC_URL!).api;
